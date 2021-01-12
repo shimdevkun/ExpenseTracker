12f3ffe432b54a6cb1dbd09fa6416adf68fa0daa
sshimabuku <shimasstrafez@gmail.com>
2021-01-12 12:34:32 -0500

    Fixed date format

--- ExpenseTracker/Models/Category.cs
+++ ExpenseTracker/Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,8 @@ namespace ExpenseTracker.Models
     public class Category
     {
         public int CategoryId { get; set; }
+
+        [DisplayName("Category")]
         public string CategoryName { get; set; }
 
         public virtual ICollection<Purchase> Purchases { get; set; }
--- ExpenseTracker/Models/Purchase.cs
+++ ExpenseTracker/Models/Purchase.cs
@@ -21,8 +21,8 @@ namespace ExpenseTracker.Models
         public decimal Cost { get; set; }
 
         [Required]
-        [DataType(DataType.DateTime, ErrorMessage = "The date must have the following format : dd/MM/yyyy")]
-        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = @"{0:dd\/MM\/yyyy}")]
+        [DataType(DataType.DateTime, ErrorMessage = "The date must have the following format : MM/dd/yyyy")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = @"{0:MM\/dd\/yyyy}")]
         public DateTime Date { get; set; }
 
         public virtual Budget Budget { get; set; }
