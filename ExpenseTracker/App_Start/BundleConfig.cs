36fd04644cb2d1945c3d2072e51377f1d32281ba
sshimabuku <shimasstrafez@gmail.com>
2021-01-15 22:13:26 -0500

    Refactoring

--- ExpenseTracker/App_Start/BundleConfig.cs
+++ ExpenseTracker/App_Start/BundleConfig.cs
@@ -14,6 +14,12 @@ namespace ExpenseTracker
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jqueryajax").Include(
+                        "~/Scripts/jquery-3.4.1.min.js",
+                        "~/Scripts/jquery.validate.min.js",
+                        "~/Scripts/jquery.validate.unobtrusive.min.js",
+                        "~/Scripts/jquery.unobtrusive-ajax.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
